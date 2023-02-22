e5b4b6042feac7c8522edac6c72546cf2b242c8b
Giorgi Zurabashvili <120266295+Chorfla@users.noreply.github.com>
2023-02-22 20:33:16 +0400

    Add files via upload

---
+++ natura.js
@@ -0,0 +1,6 @@
+let suratichange = () => {
+    document.getElementById("natura"). src = "img/natura1.png"
+}
+let suratiback = () => {
+    document.getElementById("natura"). src = "img/natura.png"
+}
